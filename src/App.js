552a54339a9081ee6861064983cb49a71eb2fe70
Morgan Snyder <msnyder@classy.org>
2017-10-10 23:36:24 -0700

    home page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import * as React from 'react';
-import logo from './logo.svg';
+import * as React from 'react'
+import glamorous from 'glamorous'
+import mandel from './mandel.svg'
+import Home from './components/Home'
 import './App.css';
+
+const Container = glamorous.div({
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '36px',
+  width: '100%'
+})
+const Content = glamorous.div({
+  textAlign: 'left',
+  width: '500px'
+})
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={mandel} className="App-logo" alt="logo" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Content>
+            <Home />
+          </Content>
+        </Container>
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import glamorous from 'glamorous'
+
+const Title = glamorous.div({
+  fontSize: '21px',
+  lineHeight: '24px',
+  marginBottom: '24px',
+  color: '#A8050E',
+})
+
+export default Title
